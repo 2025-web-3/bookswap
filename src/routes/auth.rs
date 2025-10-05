54ec7f56a0539616ed8ff33b8c6d1546b00dc82c
mr-far <88129219+mr-far@users.noreply.github.com>
2025-10-05 13:45:27 +0300

    Add `register` route without any usefulness

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,5 +1,28 @@
-use actix_web::web;
+use {
+    crate::{routes::Result, App},
+    actix_web::{web, HttpRequest, HttpResponse},
+    serde::{Deserialize, Serialize},
+};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("auth"));
+    cfg.service(web::scope("auth").route("/register", web::post().to(register)));
+}
+
+#[derive(Serialize, Deserialize)]
+struct RegisterPayload {
+    pub first_name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct RegisterResponse {
+    pub token: String,
+    pub user: String,
+}
+
+async fn register(
+    request: HttpRequest,
+    payload: web::Json<RegisterPayload>,
+    app: web::Data<App>,
+) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json("{}"))
 }
