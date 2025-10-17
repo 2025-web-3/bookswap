31e13cadc2616fbde7dba24fd851e3e38c908a8d
mr-far <88129219+mr-far@users.noreply.github.com>
2025-10-17 23:19:58 +0300

    Add sessions and new columns to user

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -4,13 +4,14 @@ CREATE TABLE IF NOT EXISTS users (
 	first_name varchar(64) NOT NULL,
 	second_name varchar(64) NOT NULL,
 	password_hash varchar NOT NULL,
+	email varchar NOT NULL,
 	school_name varchar,
 	permissions BIGINT DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
-	id BIGINT PRIMARY KEY NOT NULL UNIQUE,
 	user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-	token varchar(32),
+	token varchar(32) NOT NULL UNIQUE,
+	ip_address varchar NOT NULL,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
