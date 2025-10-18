ebd399550960233e0e31eee900630a8802372751
mr-far <88129219+mr-far@users.noreply.github.com>
2025-10-18 16:56:06 +0300

    Update schema, add new functions to ADM

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -4,14 +4,14 @@ CREATE TABLE IF NOT EXISTS users (
 	first_name varchar(64) NOT NULL,
 	second_name varchar(64) NOT NULL,
 	password_hash varchar NOT NULL,
-	email varchar NOT NULL,
+	email varchar NOT NULL UNIQUE,
 	school_name varchar,
 	permissions BIGINT DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
 	user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-	token varchar(32) NOT NULL UNIQUE,
+	token_hash varchar(32) NOT NULL UNIQUE,
 	ip_address varchar NOT NULL,
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
--- src/models/database.rs
+++ src/models/database.rs
@@ -1,4 +1,11 @@
-use {crate::models::user::User, sqlx::SqlitePool};
+use {
+    crate::{
+        models::{session::Session, user::User},
+        utils::snowflake::Snowflake,
+    },
+    sha256::digest,
+    sqlx::SqlitePool,
+};
 
 pub struct Database {
     pool: SqlitePool,
@@ -16,5 +23,48 @@ impl Database {
             .fetch_optional(&self.pool)
             .await
             .ok()?
+    }
+
+    pub async fn fetch_user_by_email(&self, email: &str) -> Option<User> {
+        sqlx::query_as!(User, "SELECT * FROM users WHERE email = $1", email)
+            .fetch_optional(&self.pool)
+            .await
+            .ok()?
+    }
+
+    pub async fn check_session_by_user_id_and_ip_exists(
+        &self, user_id: Snowflake, ip: &str,
+    ) -> bool {
+        sqlx::query_scalar!(
+            "SELECT COUNT(*) FROM sessions WHERE user_id = $1 AND ip_address = $2",
+            user_id.0,
+            ip
+        )
+        .fetch_one(&self.pool)
+        .await
+        .map_or_else(
+            |_| false,
+            |v| {
+                if v > 0 {
+                    true
+                } else {
+                    false
+                }
+            },
+        )
+    }
+
+    pub async fn fetch_session_by_token_and_ip(&self, token: &str, ip: &str) -> Option<Session> {
+        let token = digest(token);
+
+        sqlx::query_as!(
+            Session,
+            "SELECT * FROM sessions WHERE token_hash = $1 AND ip_address = $2",
+            token,
+            ip
+        )
+        .fetch_optional(&self.pool)
+        .await
+        .ok()?
     }
 }
