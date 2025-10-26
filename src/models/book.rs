7c98cf66b8ebb2845f08ffa4564e3adb5fd951da
mr-far <88129219+mr-far@users.noreply.github.com>
2025-10-27 00:04:50 +0300

    Add new routes for fetching book by id, book holders

--- src/models/book.rs
+++ src/models/book.rs
@@ -1,12 +1,45 @@
 use {
     crate::{
         routes::{HttpError, Result as HttpResult},
-        utils::snowflake::Snowflake,
+        utils::{openlibrary::get_open_library_books, snowflake::Snowflake},
     },
     chrono::NaiveDateTime,
     serde::{Deserialize, Serialize},
     sqlx::{sqlite::SqliteValueRef, Decode, Sqlite, SqliteExecutor},
 };
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum BookCondition {
+    /// The book is in perfect condition, as if new
+    MintCondition = 0,
+    /// The book is nearly perfect, with minimal signs of use
+    NearPerfect = 1,
+    /// The book has been gently used, showing minimal wear
+    GentlyUsed = 2,
+    /// The book shows signs of use, such as abrasions or lightly torn pages
+    ShowsSomeWear = 3,
+    /// The book is significantly damaged, with medium to heavy tears (even if repaired)
+    HeavilyUsed = 4,
+}
+
+impl Into<i64> for BookCondition {
+    fn into(self) -> i64 {
+        self as i64
+    }
+}
+
+impl From<i64> for BookCondition {
+    fn from(value: i64) -> BookCondition {
+        match value {
+            0 => BookCondition::MintCondition,
+            1 => BookCondition::NearPerfect,
+            2 => BookCondition::GentlyUsed,
+            3 => BookCondition::ShowsSomeWear,
+            4 => BookCondition::HeavilyUsed,
+            _ => BookCondition::MintCondition,
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Book {
@@ -40,6 +73,44 @@ impl Book {
         }
     }
 
+    pub async fn from_isbn(id: Snowflake, isbn: &str) -> HttpResult<Self> {
+        let isbnf = format!("ISBN:{}", &isbn);
+
+        let mut book_map =
+            get_open_library_books(isbnf.clone()).await.map_err(|_| HttpError::UnknownBook)?;
+        let book = book_map.remove(&isbnf).ok_or_else(|| HttpError::UnknownBook)?;
+
+        let title = book.title.as_deref().unwrap_or("No Title");
+        let description = book
+            .excerpts
+            .as_ref()
+            .and_then(|e| e.first())
+            .map(|e| e.text.as_str())
+            .unwrap_or("No Description");
+
+        let author_name = book
+            .authors
+            .as_ref()
+            .and_then(|a| a.first())
+            .map(|a| a.name.as_str())
+            .unwrap_or("Unknown Author");
+
+        let subject_name = book.subjects.and_then(|s| s.first().map(|s| s.name.clone()));
+        let cover_url = book.cover.and_then(|cover| cover.medium);
+
+        Ok(Book::new(
+            id,
+            Some(isbn.to_string()),
+            title,
+            description,
+            author_name,
+            subject_name,
+            book.number_of_pages,
+            cover_url,
+            None,
+        ))
+    }
+
     pub async fn save<'a, E: SqliteExecutor<'a>>(self, executor: E) -> HttpResult<Self> {
         sqlx::query!(r#"INSERT INTO books(id, isbn, title, description, author, subjects, pages, cover_url, publish_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)"#,
             self.id.0, self.isbn, self.title, self.description, self.author, self.subjects, self.pages, self.cover_url, self.publish_date
@@ -51,11 +122,39 @@ impl Book {
 }
 
 #[derive(Serialize)]
+pub struct BookSharingRaw {
+    pub id: Snowflake,
+    pub book_id: Snowflake,
+    pub comment: Option<String>,
+    pub holder_id: Snowflake,
+    pub condition: i64,
+}
+
+#[derive(Serialize)]
 pub struct BookSharing {
     pub id: Snowflake,
     pub book: Book,
     pub comment: Option<String>,
     pub holder_id: Snowflake,
+    pub condition: BookCondition,
+}
+
+impl BookSharing {
+    pub fn new(
+        id: Snowflake, book: Book, comment: Option<String>, holder_id: Snowflake,
+        condition: BookCondition,
+    ) -> Self {
+        Self { id, book, comment, holder_id, condition }
+    }
+
+    pub async fn save<'a, E: SqliteExecutor<'a>>(self, executor: E) -> HttpResult<Self> {
+        sqlx::query!(r#"INSERT INTO books_sharing(id, book_id, comment, holder_id ) VALUES ($1, $2, $3, $4)"#,
+            self.id.0, self.book.id.0, self.comment, self.holder_id.0
+        )
+            .execute(executor).await
+            .map(|_| self)
+            .map_err(HttpError::Database)
+    }
 }
 
 impl Decode<'_, Sqlite> for Book {
--- src/routes/books.rs
+++ src/routes/books.rs
@@ -7,12 +7,28 @@ use {
 };
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("books").route("{book_id}", web::get().to(get_book_by_id)));
+    cfg.service(
+        web::scope("books")
+            .route("{book_id}", web::get().to(get_book_by_id))
+            .route("{book_id}/holders", web::get().to(get_holders_by_book_id)),
+    );
 }
 
 async fn get_book_by_id(book_id: web::Path<i64>, app: web::Data<App>) -> Result<HttpResponse> {
     let book =
         app.database.fetch_book(book_id.to_owned().into()).await.ok_or(HttpError::UnknownBook)?;
+
+    Ok(HttpResponse::Ok().json(book))
+}
+
+async fn get_holders_by_book_id(
+    book_id: web::Path<i64>, app: web::Data<App>,
+) -> Result<HttpResponse> {
+    let book = app
+        .database
+        .fetch_holders_by_book_id(book_id.to_owned().into())
+        .await
+        .ok_or(HttpError::UnknownBook)?;
 
     Ok(HttpResponse::Ok().json(book))
 }
