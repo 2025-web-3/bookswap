4aa92a4cb07f8a39881b131020d9736b9f1d3414
Mr.Far <88129219+mr-far@users.noreply.github.com>
2025-10-28 16:00:42 +0300

    Save BookRequest to database after creation

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -153,7 +153,7 @@ async fn request_book(
             .fetch_sharing(query.sharing_id.unwrap().into())
             .await
             .ok_or(HttpError::UnknownBook)?;
-        BookRequest::new(request_id, sharing.id, sharing.book.id, me.id)
+        BookRequest::new(request_id, sharing.id, sharing.book.id, me.id).save(&app.pool).await?
     };
 
     Ok(HttpResponse::Ok().json(req))
